56027f7749c8a4a6ca0dbd7b08edd8955cf0a76c
freszek <93795323+freszek@users.noreply.github.com>
2023-04-16 12:13:39 +0200

    Warstwa Model - Powiązanie do logiki i danych - finisz?

---
+++ cal/Model/BallModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Dane;
+using Logika;
+
+namespace Model
+{
+    public class BallModel : IBallModel
+    {
+        private Kulka _kulka;
+        private LogikaAPIBase _logikaAPI;
+
+        public BallModel(Kulka kulka, LogikaAPIBase logikaAPI)
+        {
+            _kulka = kulka;
+            _logikaAPI = logikaAPI;
+        }
+
+        public override Kulka Kulka => _kulka;
+
+        public override async Task MoveAsync(double ograniczenieX, double ograniczenieY)
+        {
+            await _logikaAPI.AktualizujPolozenieKulkiAsync(_kulka, ograniczenieX, ograniczenieY);
+        }
+    }
+}
---
+++ cal/Model/IBallModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Dane;
+
+namespace Model
+{
+    public abstract class IBallModel
+    {
+        public abstract Kulka Kulka { get; }
+        public abstract Task MoveAsync(double ograniczenieX, double ograniczenieY);
+    }
+}

462d6b151267e346138952e07d57bcd3c748c636
freszek <93795323+freszek@users.noreply.github.com>
2023-04-16 11:27:01 +0200

    LogikaAPI - Połączenie projektów dane i logika, fix przestrzeni - API finisz?

---
+++ cal/Logika/ILogikaAPI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dane;
+
+namespace Logika
+{
+    public abstract class LogikaAPIBase
+    {
+        public abstract Task<List<Kulka>> PobierzKulkiAsync();
+        public abstract Task<Kulka> DodajKulkeAsync();
+        public abstract Task AktualizujPolozenieKulkiAsync(Kulka kulka, double ograniczenieX, double ograniczenieY);
+    }
+}
---
+++ cal/Logika/LogikaAPI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dane;
+
+namespace Logika
+{
+    public class LogikaAPI : LogikaAPIBase
+    {
+        private readonly DataAPIBase _dataAPI;
+
+        public LogikaAPI(DataAPIBase dataAPI)
+        {
+            _dataAPI = dataAPI;
+        }
+
+        public override Task<List<Kulka>> PobierzKulkiAsync()
+        {
+            return _dataAPI.PobierzKulkiAsync();
+        }
+
+        public override Task<Kulka> DodajKulkeAsync()
+        {
+            return _dataAPI.DodajKulkeAsync();
+        }
+
+        public override async Task AktualizujPolozenieKulkiAsync(Kulka kulka, double ograniczenieX, double ograniczenieY)
+        {
+            double nowaPozycjaX = kulka.PozycjaX + kulka.PredkoscX;
+            double nowaPozycjaY = kulka.PozycjaY + kulka.PredkoscY;
+
+            if (nowaPozycjaX < 0 || nowaPozycjaX > ograniczenieX)
+            {
+                kulka.PredkoscX = -kulka.PredkoscX;
+            }
+
+            if (nowaPozycjaY < 0 || nowaPozycjaY > ograniczenieY)
+            {
+                kulka.PredkoscY = -kulka.PredkoscY;
+            }
+
+            kulka.PozycjaX += kulka.PredkoscX;
+            kulka.PozycjaY += kulka.PredkoscY;
+
+            await Task.CompletedTask;
+        }
+    }
+}

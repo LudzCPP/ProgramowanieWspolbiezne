3def6f4f3cc0fd390df3f1e997f50cbc39b83887
Kacper Janowicz 242407 <242407@edu.p.lodz.pl>
2023-04-25 22:12:33 +0200

    ModelApi naprawione

--- cal/Model/ModelAPI.cs
+++ cal/Model/ModelAPI.cs
@@ -9,6 +9,28 @@ namespace Model
 {
     internal class ModelAPI : ModelAbstractAPI
     {
-        
+        private readonly ILogikaAPIBase logika;
+        public ModelAPI()
+        {
+            logika = ILogikaAPIBase.CreateApi();
+        }
+
+        public override void CzyscStol()
+        {
+            logika.Stop();
+            Balls.Clear();
+        }
+
+        public override void DodajKulki(int ballsNumber, int minX, int maxX, int minY, int maxY, int radius)
+        {
+            logika.DodajKulki(ballsNumber, minX, maxX, minY, maxY, radius);
+            foreach (var punkt in logika.Punkty)
+            {
+                var ball = new BallModel(punkt.X, punkt.Y, punkt.Radius);
+                punkt.BallChanged += ball.OnBallChanged;
+                Balls.Add(ball);
+            }
+            logika.Start(radius, maxX, maxY);
+        }
     }
 }

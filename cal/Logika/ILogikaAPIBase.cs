5efa709eb930dfd7c75cfed1c38d3f17687eafab
Kacper Janowicz 242407 <242407@edu.p.lodz.pl>
2023-04-25 22:06:39 +0200

    Abstrakcyjne API Logika

--- cal/Logika/ILogikaAPIBase.cs
+++ cal/Logika/ILogikaAPIBase.cs
@@ -5,7 +5,17 @@ namespace Logika
 {
     public abstract class ILogikaAPIBase
     {
+        public List<Punkt> Punkty { get; set; }
+        public abstract void DodajKulki(int ballsNumber, int minX, int maxX, int minY,
+        int maxY, int radius);
+        public abstract void Poruszanie(int radius, int maxX, int maxY);
+        public abstract void Stop();
+        public abstract void Start(int radius, int maxX, int maxY);
 
-       
+        public static ILogikaAPIBase CreateApi()
+        {
+            return new LogikaAPI();
+        }
+
     }
 }

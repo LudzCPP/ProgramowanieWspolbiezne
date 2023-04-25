23360e105672a2f655c61e19801fb7df2f73ef0e
freszek <93795323+freszek@users.noreply.github.com>
2023-04-25 22:11:16 +0200

    BallModel - Działa

--- cal/Model/BallModel.cs
+++ cal/Model/BallModel.cs
@@ -11,6 +11,47 @@ namespace Model
 {
     public class BallModel : INotifyPropertyChanged
     {
-        
+        private double x;
+        private double y;
+
+        public double X
+        {
+            get => x;
+            set
+            {
+                x = value;
+                OnPropertyChanged();
+
+            }
+        }
+        public double Y
+        {
+            get => y;
+            set
+            {
+                y = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void OnBallChanged(object sender, BallEventArgs e)
+        {
+            X = e.X;
+            Y = e.Y;
+        }
+        public double Radius { get; set; }
+
+        public BallModel(double x, double y, double radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
